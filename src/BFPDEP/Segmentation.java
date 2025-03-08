e12957ba0e800351c85657a4f6d499efaeb39c9d
HarshaPugal <harshapugazhendhi@gmail.com>
2025-03-08 12:26:16 +0530

    Committed

--- src/BFPDEP/Segmentation.java
+++ src/BFPDEP/Segmentation.java
@@ -2,9 +2,9 @@ package BFPDEP;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
-//import java.io.FileWriter;
-//import java.io.File;
-//import java.io.IOException;
+import java.io.FileWriter;
+import java.io.File;
+import java.io.IOException;
 public class Segmentation {
 	public static Map<String,ArrayList<String>> partition(Integer[] IC,String[] data,Integer halfMax) {
 		int i;
@@ -20,19 +20,19 @@ public class Segmentation {
 			}
 		}
 		
-//try {
-//	File file=new File("Database/discard.csv");
-//		FileWriter ds=new FileWriter(file,true);
-//		new FileWriter(file,false).close();
-//		for(i=0;i<discard.size();i++) {
-//			ds.write(discard.get(i)+"\n");
-//		}
-//		ds.close();
-//}
-//catch(IOException e){
-//	System.out.println("Error");
-//	e.printStackTrace();
-//}
+try {
+	File file=new File("Database/discard.csv");
+		FileWriter ds=new FileWriter(file,true);
+		new FileWriter(file,false).close();
+		for(i=0;i<discard.size();i++) {
+			ds.write(discard.get(i)+"\n");
+		}
+		ds.close();
+}
+catch(IOException e){
+	System.out.println("Error");
+	e.printStackTrace();
+}
 Map<String,ArrayList<String>> result =new HashMap<>();
 result.put("discard",discard);
 result.put("actual",actual);
